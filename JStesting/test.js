3baa795686bcd4da5e3dabb9b959b0ebd00a4fd4
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2022-05-22 23:33:58 +0300

    nothing

--- JStesting/test.js
+++ JStesting/test.js
@@ -1,16 +1,53 @@
-let logins = ["Atum", "suzuya", "flowless", "LeonidNechay"];
-document.getElementById("button").onclick = () =>
-{
-    let check = document.getElementById("agreement");
-    if(check.checked){
-        let login = document.getElementById("login").value;
-        for(i = 0; i < 4; i++)
-        {
-            if(login == logins[i])
-                alert("Цей логін вже зайнятий");
-        }
-        if(document.getElementById("login").value == document.getElementById("password").value)
-        alert("Пароль не може повторяти логін");
+//Об’єкт “Аукціон” (Код, назва лота, дата початку торгів, дата завершення торгів, 
+//стартова ціна, кінцева ціна). Запит торгів за вказану дату із початковою ціною що не перевищує Х.
+
+
+class Auction {
+    constructor(code, name, start, end, fprice, lprice) {
+      this.code = code
+      this.name = name
+      this.start = start.split(".")
+      this.end = end.split(".")
+      this.fprice = fprice
+      this.lprice = lprice
     }
-    else alert("Ви не погоджуєтеся з умовами")
-}
+  }
+  
+  class StringToLots extends Auction {
+      toString() {
+          return `
+          
+              Code: ${this.code}
+              LotName: ${this.name}
+              LotsStart: ${this.start}
+              LotsEnd: ${this.end}
+              Startprice: ${this.fprice}
+              Endprice: ${this.lprice}
+          `;
+      }
+  }
+  
+  class AllLots {
+      constructor(all_lots = []) {this.all_lots = all_lots;}
+      FindBySmth(date,price) {
+        
+          date = date.split(".")
+          for(let i = 0; i < this.all_lots.length; i++){
+              if(date[2] > this.all_lots[i].start[2] && date[2] < this.all_lots[i].end[2] && price >= this.all_lots[i].fprice){
+                return this.all_lots[i]
+              }
+              else if(date[1] > this.all_lots[i].start[1] && date[1] < this.all_lots[i].end[1] && price >= this.all_lots[i].fprice){
+                return this.all_lots[i]
+              }
+              else if(date[0] > this.all_lots[i].start[0] && date[0] < this.all_lots[i].end[0] && price >= this.all_lots[i].fprice){
+                return this.all_lots[i]
+              } 
+          }
+  
+      }
+  
+  }
+  let b = new StringToLots(1,"iphone", "20.05.2022","24.05.2022", 18000, 34000)
+  let a = new StringToLots(2, "glasess", "19.05.2022","26.05.2022", 20222, 55555)
+  let lots = new AllLots([a,b])
+  console.log(lots.FindBySmth("23.05.2022",20000))
